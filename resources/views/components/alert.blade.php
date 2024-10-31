36f69040f5075ea5783da31b74a834942c96439f
ibrahim0214 <ibrahimsonny2017@gmail.com>
2024-10-31 11:41:21 +0700

    sec update

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,4 +1,4 @@
-{{-- @props(['message'])
+@props(['message'])
 
 <div id="alert-3" class="flex items-center p-4 my-4 text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
     <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -14,4 +14,4 @@
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
       </svg>
     </button>
-  </div> --}}
+  </div>
